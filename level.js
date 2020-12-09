25266af8384b839b720cb5dc07a4f68c7adb5777
JodyFortuin <jodifortuin@gmail.com>
2020-12-09 15:36:45 +0200

    changed heading to new name

--- level.js
+++ level.js
@@ -12,6 +12,15 @@ axios.get("http://api-tutor.herokuapp.com/v1/colors").then(function (result) {
 
 });
 
+axios.get("http://localhost:2009/player").then(function (result) {
+  colorCar.innerHTML = filterTemplate({
+
+    carColor: result.data
+
+  });
+
+});
+
 
 playerBtn.addEventListener("click", function() {
   var name = nameElem.value
