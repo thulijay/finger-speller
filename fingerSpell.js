d91a11070040432157cd2852b03edf538299f27a
ammaar <fakirammaar8@gmail.com>
2020-12-09 11:55:20 +0200

    added comment

--- fingerSpell.js
+++ fingerSpell.js
@@ -1,12 +1,17 @@
 module.exports = function fingerSpell(pool) {
-function createPlayer(){
-await pool.query('insert into player values($)')
+   async function createPlayer(name) {
+        await pool.query('insert into player values($1)', [name])
+
+    }
+    async function updateProgress(){
+await pool.query('select id from ');//this is to select id so that one human is selected ,
+// to update the total attempts , successful attempts , and failed attempts.
+
 
 }
-
-
     return {
-
+        createPlayer,
+        updateProgress
 
 
     }
