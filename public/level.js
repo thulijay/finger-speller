ddb544c72fe0f92cd3f54289d849f4eaf8a108c9
JodyFortuin <jodifortuin@gmail.com>
2020-12-10 11:50:06 +0200

    commit

---
+++ public/level.js
@@ -0,0 +1,35 @@
+const playerBtn = document.querySelector(".playerBtn")
+const nameElem = document.querySelector(".nameEntered")
+
+var spellFact = fingerSpell();
+
+// axios.get("http://api-tutor.herokuapp.com/v1/colors").then(function (result) {
+//   colorCar.innerHTML = filterTemplate({
+
+//     carColor: result.data
+
+//   });
+
+// });
+
+axios.get("http://localhost:2009").then(function (result) {
+  var name = nameElem.value
+  colorCar.innerHTML = filterTemplate({
+
+    carColor: result.data
+
+  });
+
+});
+
+
+playerBtn.addEventListener("click", function() {
+  var name = nameElem.value;
+  alert('')
+console.log(name)
+   if(name !== ""){
+     spellFact.createPlayer(name)
+   }
+
+
+});
